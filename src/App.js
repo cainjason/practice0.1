c5ca915e7b465e50130b041804d9966376c18f77
Jason Cain <118203097+cainjason@users.noreply.github.com>
2024-03-20 09:40:14 -0400

    bootstraped tailwindcss

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="app">
-      <h2>Let's go</h2>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
   );
 }
